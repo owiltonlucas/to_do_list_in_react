2adb9ee6a71d99a091444b1470df67df9a5bb0b2
owiltonlucas <wiltonlucas.owl@gmail.com>
2024-11-11 16:26:54 -0300

    menu lateral e estilização do menu lateral

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
+import Menu from './components/menu/Menu';
 import Empty_state from './components/tela_inicial/Empty_state';
 
 function App() {
   return(
     <div>
       <Empty_state />
+      <Menu />
     </div>
   )
 }
--- src/components/menu/Menu.jsx
+++ src/components/menu/Menu.jsx
@@ -1,9 +1,32 @@
 import styles from './Menu.module.css'
+import Closed from '../../assets/Vector.svg'
 import React from 'react'
 
 const Menu = () => {
   return (
-    <div>Menu</div>
+    <div className={styles.menu}>
+        <button className={styles.closed}>
+            <img src={Closed} />
+        </button>
+
+        <div className={styles.menu_content}>
+            <div className={styles.input_name}>
+                <label className={styles.nome_font}> Nome </label>
+                <input type="text" className={styles.nome_content} />
+            </div>
+
+            <div className={styles.textarea_desc}>
+                <label className={styles.desc_font}> Descrição (opcional): </label>
+                <textarea className={styles.desc_content}></textarea>
+            </div>
+
+            <div className={styles.buttons}>
+                <button className={styles.fechar}>Fechar</button>
+                <button className={styles.salvar}>Salvar tarefa</button>   
+            </div>
+        </div>
+
+    </div>
   )
 }
 
